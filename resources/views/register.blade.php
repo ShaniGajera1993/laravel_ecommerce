db62b8e1d037e53c2e1c39a45e2f34d0c1678731
ShaniGajera <sgajera681@rku.ac.in>
2024-01-17 17:02:40 +0100

    [B] - Login/Register authantication done

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -23,24 +23,40 @@
             </div>
             <div class="col-md-8">
                 <div class="contact-form">
-                    <form id="contact" action="" method="post">
+                    <form id="contact" action="{{ route('auth_register') }}" method="post">
+                        @csrf
                         <div class="row">
                         <div class="col-lg-12 col-md-12 col-sm-12">
                                 <fieldset>
-                                    <input name="fullname" type="text" class="form-control" id="fullname"
-                                        placeholder="Full Name" required="">
+                                    <input name="name" type="text" class="form-control" id="name"
+                                        placeholder="Name">
+                                    @if ($errors->has('name'))
+                                        <span class="text-danger">{{ $errors->first('name') }}</span><br><br>
+                                    @endif
                                 </fieldset>
                             </div>
                             <div class="col-lg-12 col-md-12 col-sm-12">
                                 <fieldset>
                                     <input name="email" type="text" class="form-control" id="email"
-                                        placeholder="E-Mail Address" required="">
+                                        placeholder="E-Mail Address">
+                                    @if ($errors->has('email'))
+                                        <span class="text-danger">{{ $errors->first('email') }}</span><br><br>
+                                    @endif
                                 </fieldset>
                             </div>
                             <div class="col-lg-12 col-md-12 col-sm-12">
                                 <fieldset>
                                     <input name="password" type="text" class="form-control" id="password"
-                                        placeholder="Password" required="">
+                                        placeholder="Password">
+                                    @if ($errors->has('password'))
+                                        <span class="text-danger">{{ $errors->first('password') }}</span><br><br>
+                                    @endif
+                                </fieldset>
+                            </div>
+                            <div class="col-lg-12 col-md-12 col-sm-12">
+                                <fieldset>
+                                    <input name="password_confirmation" type="text" class="form-control" id="password"
+                                        placeholder="Confirm Password">
                                 </fieldset>
                             </div>
                             <div class="col-lg-12">
